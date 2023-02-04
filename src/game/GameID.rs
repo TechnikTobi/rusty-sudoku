18457741841d0d9aee1245940993008e9118c7a2
Tobias Prisching <tobias.prisching@icloud.com>
2023-02-04 13:40:05 +0100

    Create GameID.rs

---
+++ src/game/GameID.rs
@@ -0,0 +1,19 @@
+use std::{sync::atomic::{AtomicUsize, Ordering}};
+
+#[derive(PartialEq, Eq, Hash)]
+pub struct
+GameID
+{
+	value: usize
+}
+
+static GameIDcounter: AtomicUsize = AtomicUsize::new(1);
+
+impl GameID
+{
+	pub fn new() -> Self
+	{ GameID { value: GameIDcounter.fetch_add(1, Ordering::Relaxed) } }
+
+	pub fn as_str (&self) -> String
+	{ self.value.to_string() }
+}
