73ae7aa3be472d924cbc3745729165071e5a4789
Tobias Prisching <tobias.prisching@icloud.com>
2023-02-04 01:03:53 +0100

    Moving endpoints to separate file

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,10 @@
+mod endpoints;
+
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
-#[get("/")]
-async fn hello() -> impl Responder {
-	HttpResponse::Ok().body("Hello world!")
-}
+use crate::endpoints::*;
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-	HttpResponse::Ok().body(req_body)
-}
 
-async fn manual_hello() -> impl Responder {
-	HttpResponse::Ok().body("Hey there!")
-}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
