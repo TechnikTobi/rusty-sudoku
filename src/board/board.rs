70d58bc32f0f6c4561063085937c07b364de4037
Tobias Prisching <tobias.prisching@icloud.com>
2023-02-04 01:34:42 +0100

    Create board.rs

---
+++ src/board/board.rs
@@ -0,0 +1,31 @@
+use crate::board::field::*;
+
+pub struct
+Board
+{
+	fields: Vec<Field>
+}
+
+static MAX_X: u32 = 9;
+static MAX_Y: u32 = 9;
+
+impl Board
+{
+
+	pub fn
+	new()
+	-> Self
+	{
+
+
+		let mut fields = Vec::new();
+		(0..MAX_Y).for_each(|y| 
+			(0..MAX_X).for_each(|x| 
+				{ fields.push(Field::new(x, y, 0)); } 
+			) 
+		);
+
+		Board { fields }
+	}
+
+}
