35bfc28d2af5d2e5ea7b06911c098a71e6810c84
Tobias Prisching <tobias.prisching@icloud.com>
2023-02-06 18:58:49 +0100

    Update Game.rs

--- src/game/Game.rs
+++ src/game/Game.rs
@@ -1,7 +1,35 @@
 use crate::game::GameID::GameID;
+use crate::board::board::Board;
 
 pub struct
 Game
 {
-	id: GameID
+	id: GameID,
+	name: String,
+	difficulty: u8	
+}
+
+impl
+Game
+{
+	pub fn
+	new
+	(
+		name: String,
+		difficulty: u8
+	)
+	-> Self
+	{
+		Game
+		{
+			id: GameID::new(),
+			name: name,
+			difficulty: Board::bound_difficulty(difficulty)
+		}
+	}
+
+	pub fn get_game_id (&self) -> &GameID { &self.id }
+	pub fn get_name (&self) -> &String { &self.name }
+	pub fn get_difficulty ( &self )	-> &u8 { &self.difficulty }
+
 }
