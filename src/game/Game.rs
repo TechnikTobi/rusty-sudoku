3336aa860f159c945c98e5445558a082c0cd79f0
Tobias Prisching <tobias.prisching@icloud.com>
2023-02-07 22:51:10 +0100

    Update Game.rs

--- src/game/Game.rs
+++ src/game/Game.rs
@@ -1,12 +1,15 @@
 use crate::game::GameID::GameID;
 use crate::board::board::Board;
 
+use super::EGameState::EGameState;
+
 pub struct
 Game
 {
 	id: GameID,
 	name: String,
-	difficulty: u8	
+	difficulty: u8,
+	state: EGameState,
 }
 
 impl
@@ -24,12 +27,24 @@ Game
 		{
 			id: GameID::new(),
 			name: name,
-			difficulty: Board::bound_difficulty(difficulty)
+			difficulty: Board::bound_difficulty(difficulty),
+			state: EGameState::READY
 		}
 	}
 
-	pub fn get_game_id    (&self) -> &GameID { &self.id }
-	pub fn get_name       (&self) -> &String { &self.name }
-	pub fn get_difficulty (&self) -> &u8     { &self.difficulty }
+	pub fn get_game_id    (&self) -> &GameID     { &self.id }
+	pub fn get_name       (&self) -> &String     { &self.name }
+	pub fn get_difficulty (&self) -> &u8         { &self.difficulty }
+	pub fn get_state      (&self) -> &EGameState { &self.state }
+
+	pub fn 
+	set_state
+	(
+		&mut self, 
+		new_state: EGameState
+	)
+	{
+		self.state = new_state;
+	}
 
 }
