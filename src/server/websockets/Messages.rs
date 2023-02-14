91dcbca7ff4d5f7d3ec67bb2db37565b38f80a9b
Tobias Prisching <tobias.prisching@icloud.com>
2023-02-14 05:15:34 +0100

    Removing old Messages

--- src/server/websockets/Messages.rs
+++ src/server/websockets/Messages.rs
@@ -1,42 +1,15 @@
 use actix::prelude::*;
 
 use crate::messages::base::NetworkPlayerIdentifier::NetworkPlayerIdentifier;
-use crate::messages::incoming::GameCreationRequest::GameCreationRequest;
-use crate::messages::incoming::PlayerRegistrationRequest::PlayerRegistrationRequest;
-
 use crate::messages::outgoing::GamesListResponse::GamesListResponse;
 
-// // INCOMING
-// #[derive(Clone, Message)]
-// #[rtype(result = "()")]
-// pub struct WebsocketPlayerRegistrationRequest(pub PlayerRegistrationRequest);
-
-// #[derive(Clone, Message)]
-// #[rtype(result = "()")]
-// pub struct WebsocketGameCreationRequest(pub GameCreationRequest);
-
-
-
-// // OUTGOING
-// #[derive(Clone, Message)]
-// #[rtype(result = "()")]
-// pub struct WebsocketPlayerRegistrationResponse(pub PlayerRegistrationRequest);
-
-// #[derive(Clone, Message, Debug)]
-// #[rtype(result = "()")]
-// pub struct WebsocketGamesList(pub GamesListResponse);
-
-// #[derive(Clone, Message, Debug)]
-// #[rtype(result = "()")]
-// pub struct WebsocketGameUpdate();
-
-
-
+// IN- AND OUTGOING
 #[derive(Clone, Message, Debug)]
 #[rtype(result = "()")]
 // pub struct JsonMessage(pub String);
 pub struct JsonMessage(pub String, pub Option<Recipient<JsonMessage>>);
 
+// INTERNAL
 #[derive(Clone, Message, Debug)]
 #[rtype(result = "()")]
 pub struct InternalPlayerRegistrationMessage(pub NetworkPlayerIdentifier, pub Recipient<JsonMessage>);
