99dd495f34ef9bd0d9a9e055d96a30e466aa4b3c
Tobias Prisching <tobias.prisching@icloud.com>
2023-02-14 05:15:53 +0100

    Moving the Websocket Server into its own file

--- src/server/websockets/Session.rs
+++ src/server/websockets/Session.rs
@@ -1,19 +1,16 @@
 use actix::{fut, prelude::*};
-use actix_broker::{BrokerIssue, BrokerSubscribe};
 use actix_web::web;
 use actix_web_actors::ws;
 
-use std::{sync::Mutex, collections::HashMap};
+use std::sync::Mutex;
 
 use crate::game::player::PlayerID::PlayerID;
-use crate::messages::outgoing::PlayerRegistrationResponse::PlayerRegistrationResponse;
 use crate::server::Server::SudokuServer;
-use crate::messages::base::NetworkPlayerIdentifier::NetworkPlayerIdentifier;
-use crate::messages::base::NetworkGameIdentifier::NetworkGameIdentifier;
 use crate::messages::incoming::GameCreationRequest::GameCreationRequest;
 use crate::messages::incoming::PlayerRegistrationRequest::PlayerRegistrationRequest;
 
 use super::Messages::*;
+use super::Server::WebSocketServer;
 
 #[derive(Default)]
 pub struct
@@ -176,121 +173,5 @@ WebsocketSession
 			}
 			_ => {}
 		}
-	}
-}
-
-
-
-
-#[derive(Default)]
-pub struct
-WebSocketServer
-{
-	// clients: HashMap<NetworkPlayerIdentifier, Recipient<WebsocketGamesList>>,
-	// games_clients: HashMap<NetworkGameIdentifier, Vec<Recipient<GameUpdateMessage>>>,
-	clients: HashMap<NetworkPlayerIdentifier, Recipient<JsonMessage>>,
-}
-
-impl
-WebSocketServer
-{
-	pub fn 
-	send_to_all
-	(
-		&self,
-		message: JsonMessage
-	)
-	{
-		for (_, client) in &self.clients
-		{
-			client.try_send(message.to_owned());
-		}
-	}
-}
-
-impl SystemService for WebSocketServer {}
-impl Supervised for WebSocketServer {}
-
-impl 
-Actor 
-for 
-WebSocketServer 
-{
-	type Context = Context<Self>;
-
-	fn 
-	started
-	(
-		&mut self, 
-		context: &mut Self::Context
-	) 
-	{
-		self.subscribe_system_async::<InternalPlayerRegistrationMessage>(context);
-		self.subscribe_system_async::<InternalGameCreationMessage>(context);
-	}
-}
-
-impl 
-Handler<InternalPlayerRegistrationMessage> 
-for 
-WebSocketServer 
-{
-	type Result = MessageResult<InternalPlayerRegistrationMessage>;
-
-	fn handle(
-		&mut self, 
-		msg: InternalPlayerRegistrationMessage, 
-		_ctx: &mut Self::Context
-	) 
-	-> Self::Result 
-	{
-		// Deconstruct the internal message
-		let InternalPlayerRegistrationMessage(network_player_id, recipient) = msg;
-
-		// Add the recipient to the list of clients
-		self.clients.insert(network_player_id.clone(), recipient.clone());
-		
-		// Send them back their player ID
-		let send_result = recipient.try_send(JsonMessage(
-			serde_json::to_string(&PlayerRegistrationResponse::new(network_player_id, "".to_string())).unwrap(), 
-			None
-		));
-
-		// If that results in an error, currently handle this by printing something to CLI
-		if let Err(error) = send_result
-		{
-			println!("Oh no! Something went wrong with sending the new player their ID! {:?}", error);
-		}
-
-		// Still not sure why we return... this
-		MessageResult(())
-	}
-}
-
-impl 
-Handler<InternalGameCreationMessage> 
-for 
-WebSocketServer 
-{
-	type Result = MessageResult<InternalGameCreationMessage>;
-
-	fn handle(
-		&mut self, 
-		msg: InternalGameCreationMessage, 
-		_ctx: &mut Self::Context
-	) 
-	-> Self::Result 
-	{
-		// Deconstruct the internal message
-		let InternalGameCreationMessage(games_list) = msg;
-
-		// Send to EVERYONE
-		self.send_to_all(JsonMessage(
-			serde_json::to_string(&games_list).unwrap(), 
-			None
-		));
-
-		// See above
-		MessageResult(())
 	}
 }
--- src/server/websockets/mod.rs
+++ src/server/websockets/mod.rs
@@ -1,2 +1,8 @@
+#[allow(non_snake_case)]
 pub mod Messages;
+
+#[allow(non_snake_case)]
 pub mod Session;
+
+#[allow(non_snake_case)]
+pub mod Server;
