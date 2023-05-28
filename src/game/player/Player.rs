2800c2ec3d9d4aa6dd09177dc09b7327b5b165c7
Tobias Prisching <tobias.prisching@icloud.com>
2023-05-28 14:49:15 +0200

    Added token to player

--- src/game/player/Player.rs
+++ src/game/player/Player.rs
@@ -2,14 +2,17 @@ use std::time::Instant;
 
 use crate::{game::player::PlayerID::PlayerID, color::Color::Color, messages::base::PlayerListElement::PlayerListElement};
 
+use super::PlayerToken;
+
 #[derive(Debug, Clone)]
 pub struct
 Player
 {
-	id: PlayerID,
-	color: Color,
-	name: String,
-	creation_time: Instant,
+	id:                                PlayerID,
+	token:                             PlayerToken,
+	color:                             Color,
+	name:                              String,
+	creation_time:                     Instant,
 }
 
 impl
@@ -24,17 +27,19 @@ Player
 	{
 		Player 
 		{ 
-			id: PlayerID::new(), 
-			color: Color::new_random_color(), 
-			name: name,
-			creation_time: Instant::now()
+			id:                        PlayerID::new(), 
+			token:                     PlayerToken::new(),
+			color:                     Color::new_random_color(), 
+			name:                      name,
+			creation_time:             Instant::now()
 		}
 	}
 
-	pub fn get_player_id (&self) -> &PlayerID { &self.id }
-	pub fn get_color     (&self) -> &Color    { &self.color }
-	pub fn get_name      (&self) -> &String   { &self.name }
-	pub fn get_age       (&self) -> u64       { self.creation_time.elapsed().as_secs() }
+	pub fn get_player_id (&self) -> &PlayerID    { &self.id }
+	pub fn get_token     (&self) -> &PlayerToken { &self.token }
+	pub fn get_color     (&self) -> &Color       { &self.color }
+	pub fn get_name      (&self) -> &String      { &self.name }
+	pub fn get_age       (&self) -> u64          { self.creation_time.elapsed().as_secs() }
 
 	pub fn
 	to_network
